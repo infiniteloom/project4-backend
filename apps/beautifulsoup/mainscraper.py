547a954a83cfc405871ef312d767ac5f8e11c422
LEA THOMAS <LEATHOMASLT@GMAIL.COM>
2020-09-16 11:40:12 -0400

    refactored address scraper

--- apps/beautifulsoup/mainscraper.py
+++ apps/beautifulsoup/mainscraper.py
@@ -1,27 +1,42 @@
 import bs4 as bs
 import urllib.request
 import csv
-import re
-from selenium import webdriver
+
+# import re
+# from selenium import webdriver
+
+
+"""
+This file opens each individual house's URL on the imported list of URLs from urlscraper.py (a file that 
+scrapes href tags from the thumbnails as well as img srcs of the thumbnails of homes on Compass's regional listings page)
+Each row in the final resulting 'house-listings.csv' file is representative of all descriptive information scraped from 
+each individual home. 
+
+
+with open('listings-urls.csv', 'r') as csv_file:
+    csvreader = csv.reader(csv_file)
+
+    # Skips header line in csv_file.
+    next(csvreader)
 
 
 
+    # Loop through this entire scraping function per row (contains single listing URL) in csv_file
+    for row in csvreader:
+        print(row[0])
 
 
-img_page = urllib.request.urlopen('https://www.compass.com/listing/1084-highway-55a-napanoch-ny-12458/591790390221494241/').read()
-image_soup = bs.BeautifulSoup(img_page, features="html.parser")
-image_soup_b = image_soup.body
-#print(image_soup_b)
+        info_page = urllib.request.urlopen(row[0]).read()
+"""
+info_page = urllib.request.urlopen('https://www.compass.com/listing/174-pacific-street-unit-4a-brooklyn-ny-11201/529504371236159209/').read()
 
-
-info_page = urllib.request.urlopen('https://www.compass.com/listing/1084-highway-55a-napanoch-ny-12458/591790390221494241/').read()
 info_soup = bs.BeautifulSoup(info_page, features="html.parser")
 body = info_soup.body
 table = info_soup.find('table', class_="data-table__TableStyled-ibnf7p-0 bhHpMT")
 table_rows = table.find_all('tr')
-info = body.find_all('div', class_="property-information__FieldSection-sc-1il5vdr-7")
 
 
+# This is the dictionary that holds all of the gathered information and will be appended to the csv.
 house = {
     "type": '',
     "city": '',
@@ -42,8 +57,13 @@ house = {
 }
 
 # Get description
-house['description'] = body.find('div', class_="sc-fzoWqW eoVQRn").text
+#house['description'] = body.find('div', class_="sc-fzoWqW eoVQRn").text
 
+
+info = body.select('div[class*="summary__SummaryCaption"]')
+
+# address_tags = body.find_all('a', class_="textIntent-caption1")
+# house['street'] = address_tags[4:5]
 
 # Get price
 price_poss = body.find_all('div', class_="textIntent-title2")
@@ -55,12 +75,14 @@ house['price'] = price[0]
 
 
 # Get address
-house['street'] = body.find('p', class_="summary__StyledAddress-e4c4ok-6").text
-address = body.find('div', class_="summary__StyledAddressCaption-e4c4ok-7").text
-split = address.split()
-house['city'] = split[0][:-1]
-house['state'] = split[1]
-house['zip'] = split[2]
+address = body.select('div[class*="summary__StyledAddressCaption"]')
+address_text = address[0].text
+addr_split = address_text.split(",")
+house['street'] = addr_split[0]
+house['city'] = addr_split[1].strip(' ')
+house['state'] = (addr_split[2].lstrip(' ').split(' '))[0]
+house['zip'] = (addr_split[2].lstrip(' ').split(' '))[1]
+
 
 
 # Get home_size, bed, bath
@@ -77,12 +99,14 @@ for inf in info:
 # Get year_built, lot_size, type, county
 for tr in table_rows:
     td = tr.find_all('td')
-    for i in range (len(td)-1):
+    for i in range(len(td)-1):
         if td[i].text == 'Year Built':
             house['year_built'] = int(td[i+1].text)
         if td[i].text == 'Lot Size':
             split = td[i + 1].text.split(" ", 1)
             house['lot_size'] = float(split[0])
+            # needs to remove the comma, if it exists:
+            # https://www.compass.com/listing/552-1st-street-brooklyn-ny-11215/460178852678502065/
         if td[i].text == "Compass Type":
             house['type'] = td[i+1].text
         if td[i].text == "County":
@@ -99,5 +123,4 @@ file = csv.writer(open('house-listings.csv', 'w'))
 file.writerow(["type", "city", "state", "zip", "street", "year_built", "bed", "bath", "home_size", "lot_size", "price", "description", "image1", "image2", "image3", "image4"])
 
 # Add this house as a row to the csv.
-file.addrow([house['type'], house['city'], house['state'], house['zip'], house['street'], house['year_built'], house['bed'], house['bath'], house['home_size'], house['lot_size'], house['price'], house['description'], house['image1'], house['image2'], house['image3'], house['image4']])
-
+file.writerow([house['type'], house['city'], house['state'], house['zip'], house['street'], house['year_built'], house['bed'], house['bath'], house['home_size'], house['lot_size'], house['price'], house['description'], house['image1'], house['image2'], house['image3'], house['image4']])
--- apps/beautifulsoup/urlscraper.py
+++ apps/beautifulsoup/urlscraper.py
@@ -1,16 +1,42 @@
 import bs4 as bs
 import urllib.request
 import csv
-import re
 
 
 """
 This function scrapes the thumbnails page of regional listings and gathers the urls of each individual listing
 which will then be passed along to the main scraper.py to retrieve individual house listings and add them to a seed data file "house-listings.csv"
 """
-# make a scraper that collects the urls of of all a tags with the that class
-# grab href and create a list of all the urls
-# run a loop through the list
-# run this code per the list
 
+# Create a csv file to write to, add the header
+file = csv.writer(open('listings-urls.csv', 'w'))
+file.writerow(['url'])
+
+# Gather primary information from body of multiple regional listings page.
+
+regional_listings_pages = [
+    'https://www.compass.com/homes-for-sale/_map/mapview=41.69125189087717,-73.67678056005388,41.23070881554308,-73.99675736669451/price.min=425k/',
+    'https://www.compass.com/homes-for-sale/_map/mapview=41.31987607858595,-73.86880184751635,40.390254627043724,-74.5087554607976/price.min=425k/',
+    'https://www.compass.com/homes-for-sale/_map/mapview=42.75937320781332,-74.61276769085157,41.84936884760099,-75.25409459514843/price.min=425k/',
+    'https://www.compass.com/homes-for-sale/_map/mapview=40.94646206869337,-73.78396725910888,40.713963575871645,-73.94395566242919/price.min=425k/',
+    'https://www.compass.com/homes-for-sale/_map/mapview=40.739876697212324,-73.90153245345559,40.62336718364866,-73.98152665511574/price.min=425k/',
+    'https://www.compass.com/homes-for-sale/_map/mapview=41.95950103394698,-74.30267513287292,41.50087486692388,-74.62265193951355/price.min=425k/',
+    'https://www.compass.com/homes-for-sale/_map/mapview=42.13384557493043,-73.04956830745847,41.21582231944765,-73.68952192073972/price.min=425k/',
+    'https://www.compass.com/homes-for-sale/_map/mapview=41.410202223388396,-72.31369478478442,41.179340280337904,-72.47368318810473/price.min=900k/',
+    'https://www.compass.com/homes-for-sale/_map/mapview=40.866261044035014,-73.86248188690134,40.63348106991663,-74.02247029022165/price.min=2m/',
+    'https://www.compass.com/homes-for-sale/_map/mapview=41.60845759224359,-73.88417588277268,41.147324927495916,-74.2041526894133/price.min=2m/',
+    'https://www.compass.com/homes-for-sale/_map/mapview=42.02310014075104,-74.4249357604212,41.56492994464449,-74.74491256706183/price.min=2m/'
+]
+for page in regional_listings_pages:
+    info_page = urllib.request.urlopen(page).read()
+    info_soup = bs.BeautifulSoup(info_page, features="html.parser")
+    body = info_soup.body
+
+
+    # Gather all the URL links for individual house listings
+    # Must add 'https://compass.com to the beginning of all gathered URLs
+    for link in body.find_all('a', class_="uc-listingPhotoCard uc-listingCard uc-listingCard-has-photo"):
+        ext = link.get('href')
+        fullurl = ('https://www.compass.com' + ext)
+        file.writerow([fullurl])
 
