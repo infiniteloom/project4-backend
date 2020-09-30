6fc2bda3a89c93c7566c9e08c196b77235fbf8c3
LEA THOMAS <LEATHOMASLT@GMAIL.COM>
2020-09-30 17:31:51 -0400

    updated read me

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -12,19 +12,3 @@ class ListingSerializer(serializers.ModelSerializer):
         model = Listing
         fields = '__all__'
 
-"""
-class BuyerSerializer(serializers.ModelSerializer):
-   # favorites = serializers.ReadOnlyField(source='favorites.street' + 'favorites.city' + 'favorites.state + favorites.zip') #########
-
-    class Meta:
-        model = BuyerUser
-        fields = '__all__'
-
-
-class RealtorSerializer(serializers.ModelSerializer):
-    #listings = ListingSerializer(many=True, read_only=True, required=False)
-
-    class Meta:
-        model = RealtorUser
-        fields = '__all__'
-"""
